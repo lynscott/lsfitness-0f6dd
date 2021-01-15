33a9039241f7a0bfef47c07a0bc057fe2029067f
lyn <lennord@gmail.com>
2021-01-15 09:28:13 -0800

    added some images and custom headings

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -11,11 +11,11 @@ export default class SectionHero extends React.Component {
             <section id={_.get(section, 'section_id', null)} className="block hero-block bg-accent outer">
               <div className="inner">
                 <div className="grid">
-                  {_.get(section, 'image', null) && (
+                  {/* {_.get(section, 'image', null) && (
                   <div className="cell block-preview">
                     <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
                   </div>
-                  )}
+                  )} */}
                   <div className="cell block-content">
                     {_.get(section, 'title', null) && (
                     <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
